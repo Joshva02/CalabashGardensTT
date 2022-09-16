1a5d84710cfe728c14d899ddfc353ef009b13528
Joshva02 <109680659+Joshva02@users.noreply.github.com>
2022-09-16 11:02:41 -0400

    Trying call button

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 import { SubHeading } from "../../components"
-import { images, data } from "../../constants"
+import { images } from "../../constants"
 
 const FindUs = () => (
   <div className="app__bg app__wrapper section__padding" id="contact">
@@ -12,7 +12,7 @@ const FindUs = () => (
       </h1>
       <div className="app__wrapper-contact">
         <p className="p__opensans">
-          Lane Ends Bungalow, Whatcroft Hall Lane, Rudheath, CW9 7SG
+        48 Tragarete Rd, Port of Spain
         </p>
         <p
           className="p__cormorant"
@@ -24,7 +24,9 @@ const FindUs = () => (
         <p className="p__opensans">Sat - Sun: 10:00 am - 03:00 am</p>
       </div>
       <button className="custom__button" style={{ marginTop: "2rem" }}>
-        Visit Us
+      <a href="tel:+18684798164">
+        Call Us!
+        </a>
       </button>
     </div>
     <div className="app__wrapper_img">
